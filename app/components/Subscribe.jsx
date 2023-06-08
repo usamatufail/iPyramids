4de095cbdbcbcef47e160eb9590d80c88c0e39c3
Usama Tufail <usamatufail@Usamas-MacBook-Pro-2.local>
2023-06-08 15:42:26 -0700

    style: update features and subscribe

--- app/components/Subscribe.jsx
+++ app/components/Subscribe.jsx
@@ -5,8 +5,10 @@ import {CustomButton} from './CustomButton';
 export const Subscribe = () => {
   // const [email, setEmail] = useState('');
   return (
-    <section className="grid min-h-[400px] md:grid-cols-2 gap-[50px] items-center bg-[url('https://cdn.shopify.com/s/files/1/0771/2768/0277/files/mail-bg.png')] px-[20px] bg-cover bg-no-repeat py-[40px]">
-      <div className=" flex flex-col gap-[32px] m-auto justify-center items-center md:items-start">
+    <section className="grid min-h-[400px] md:grid-cols-2 gap-[50px] items-center bg-[url('https://cdn.shopify.com/s/files/1/0771/2768/0277/files/pexels-philippe-donn-1169754_1.jpg')] px-[20px] bg-cover relative bg-center bg-no-repeat py-[40px]">
+      <div className="h-full w-full bg-[rgba(0,0,0,0.5)] absolute top-0 opacity-70 z-10" />
+
+      <div className=" flex flex-col gap-[32px] m-auto justify-center items-center md:items-start relative z-20">
         <Animate>
           <h2 className="text-[28px] md:text-[42px] font-[700] text-white">
             Subscribe Now
@@ -32,7 +34,7 @@ export const Subscribe = () => {
           /> */}
         </Zoom>
       </div>
-      <div className="flex justify-center">
+      <div className="flex justify-center relative z-20">
         <Zoom>
           <img
             src="/home/diamond.png"
--- app/containers/home/features.client.jsx
+++ app/containers/home/features.client.jsx
@@ -15,13 +15,13 @@ const data = [
   },
   {
     src: '/svg/brain.svg',
-    text: 'Unleash Higher Consciousness',
+    text: 'Higher Consciousness',
   },
 ];
 
 export const Features = () => {
   return (
-    <section className="min-h-[450px] bg-[url('https://cdn.shopify.com/s/files/1/0771/2768/0277/files/features-bg.png')] bg-cover bg-center bg-no-repeat flex flex-col gap-[12px] items-center justify-center py-[50px]">
+    <section className="min-h-[450px] bg-[url('https://cdn.shopify.com/s/files/1/0771/2768/0277/files/features-bg.png')] bg-cover bg-center bg-no-repeat flex flex-col gap-[12px] items-center justify-center py-[50px] px-[10px]">
       <Animate>
         <h1 className="text-[28px] md:text-[48px] font-[500] text-white">
           Coherence Within The Mind
@@ -29,7 +29,7 @@ export const Features = () => {
       </Animate>
       <Animate>{/* <CustomButton /> */}</Animate>
       <Zoom>
-        <div className="grid grid-cols-2 md:grid-cols-4 justify-center items-center gap-[50px] mt-[30px]">
+        <div className="grid grid-cols-4 justify-center items-center gap-[15px] md:gap-[50px] mt-[30px]">
           {data.map((data) => {
             return (
               <div key={data.text}>
@@ -45,11 +45,14 @@ export const Features = () => {
 
 const Card = ({text, src}) => {
   return (
-    <div className=" bg-transparent relative flex flex-col gap-[50px] items-center justify-center rounded-[128px] py-[30px]">
-      <div className="h-full w-full bg-[rgba(0,0,0,0.5)] absolute top-0 opacity-30 rounded-[128px] z-10" />
-      <div className="flex flex-col gap-[50px] items-center justify-center relative z-30 rounded-[128px]">
-        <img src={src} alt={text} />
-        <h1 className="text-[16px] font-[700] text-white text-center max-w-[105px] md:max-w-[180px] max-h-[40px]">
+    <div className=" bg-transparent flex flex-col gap-[50px] items-center justify-center rounded-[128px] py-[30px]">
+      <div className="flex flex-col gap-[20px] md:gap-[50px] items-center justify-center">
+        <img
+          src={src}
+          alt={text}
+          className="w-[50px] h-[50px] md:w-[70px] md:h-[70px]"
+        />
+        <h1 className="text-[11px] md:text-[16px] font-[600] md:font-[700] text-white text-center md:max-w-[180px] max-h-[40px]">
           {text}
         </h1>
       </div>
